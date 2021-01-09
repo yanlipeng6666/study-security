dfa6a0a9eae05a03b49fef89f59fbd73c1ac600a
yanlp <85827444@qq.com>
2021-01-09 23:17:09 +0800

    可以从SecurityContextHolder获取用户信息

--- security-spring-boot/src/main/java/com/itheima/security/springboot/controller/LoginController.java
+++ security-spring-boot/src/main/java/com/itheima/security/springboot/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.itheima.security.springboot.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +24,37 @@ public class LoginController {
      */
     @RequestMapping(value = "/login-success", produces = {"text/plain;charset=UTF-8"})
     public String loginSuccess() {
-        return "登录成功";
+        return getUserName() + " 登录成功";
     }
 
     @RequestMapping(value = "/r/r1", produces = {"text/plain;charset=UTF-8"})
     public String r1() {
-        return "访问资源1";
+        return getUserName() + "访问资源1";
     }
 
     @RequestMapping(value = "/r/r2", produces = {"text/plain;charset=UTF-8"})
     public String r2() {
-        return "访问资源2";
+        return getUserName() + "访问资源2";
+    }
+
+    /**
+     * 获取SpringSecurity的userName
+     * @return
+     */
+    private String getUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        String username = "";
+        if (principal == null) {
+            username = "匿名";
+        } else {
+            if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+                username = userDetails.getUsername();
+            } else {
+                username = principal.toString();
+            }
+        }
+        return username;
     }
 }
