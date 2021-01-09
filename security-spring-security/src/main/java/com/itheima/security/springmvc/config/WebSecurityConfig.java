900845ee3fd6f6638a0b3a3d145069c7c3e16253
yanlp <85827444@qq.com>
2021-01-09 16:23:10 +0800

    SpringSecurity安全配置

---
+++ security-spring-security/src/main/java/com/itheima/security/springmvc/config/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.itheima.security.springmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * Program Name: security-spring-security
+ * Created by yanlp on 2021-01-09
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 配置用户信息服务
+     *
+     * @return
+     */
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
+        manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
+
+        return manager;
+    }
+
+    /**
+     * 密码编码器
+     *
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    /**
+     * 配置安全拦截机制(最重要)
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/r/**").authenticated()// 所有antMatchers表明的请求必须认证
+                .anyRequest().permitAll()// 除了antMatchers,其他请求可以访问
+                .and()
+                .formLogin()// 允许表单登录
+                .successForwardUrl("/login-success");// 自定义登录成功的页面地址
+    }
+}
--- security-spring-security/src/main/java/com/itheima/security/springmvc/init/SpringApplicationInitializer.java
+++ security-spring-security/src/main/java/com/itheima/security/springmvc/init/SpringApplicationInitializer.java
@@ -2,9 +2,11 @@ package com.itheima.security.springmvc.init;
 
 import com.itheima.security.springmvc.config.ApplicationConfig;
 import com.itheima.security.springmvc.config.WebConfig;
+import com.itheima.security.springmvc.config.WebSecurityConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
+ * Spring容器初始化
  * Program Name: security-springmvc
  * Created by yanlp on 2021-01-08
  *
@@ -15,15 +17,17 @@ public class SpringApplicationInitializer extends AbstractAnnotationConfigDispat
 
     /**
      * spring容器,相当于加载 applicationContext.xml
+     *
      * @return
      */
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{ApplicationConfig.class};
+        return new Class<?>[]{ApplicationConfig.class, WebSecurityConfig.class};
     }
 
     /**
      * servletContext,相当于加载springmvc.xml
+     *
      * @return
      */
     @Override
