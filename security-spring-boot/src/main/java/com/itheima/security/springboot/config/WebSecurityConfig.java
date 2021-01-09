bcbfd43c317237c444651463ff6a68d061e8dd65
yanlp <85827444@qq.com>
2021-01-09 18:40:36 +0800

    使用自己写的 UserDetailService,到时候可以自己从数据库里查询判断

--- security-spring-boot/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
+++ security-spring-boot/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
@@ -4,11 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Program Name: security-spring-security
@@ -25,15 +22,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      *
      * @return
      */
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
-        manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
-
-        return manager;
-    }
+    // @Bean
+    // @Override
+    // public UserDetailsService userDetailsService() {
+    //     InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    //     manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
+    //     manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
+    //
+    //     return manager;
+    // }
 
     /**
      * 密码编码器
@@ -43,6 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+        // return new BCryptPasswordEncoder();
     }
 
     /**
---
+++ security-spring-boot/src/main/java/com/itheima/security/springboot/service/SpringDataDetailService.java
@@ -0,0 +1,28 @@
+package com.itheima.security.springboot.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Program Name: security-itheima
+ * Created by yanlp on 2021-01-09
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+@Service
+public class SpringDataDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 根据账号去数据库查询...
+        // 这里暂时使用静态数据 UserDetails userDetails
+        System.out.println("用户名: " + username);
+
+        UserDetails userDetails = User.withUsername("zhangsan").password("123").authorities("p1").build();
+
+        return userDetails;
+    }
+}
