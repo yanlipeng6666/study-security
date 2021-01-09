b26069b2da0a0d7d229d9a2f16932360389412d3
yanlp <85827444@qq.com>
2021-01-09 22:24:44 +0800

    1.登录页面使用自定义,注意一定要加.permitAll() 2.如果springboot访问jsp,pom里一定在build标签下代码那么写,否则访问不到jsp

--- security-spring-boot/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
+++ security-spring-boot/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
@@ -51,13 +51,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()// 不再限制CSRF
+                .authorizeRequests()
                 .antMatchers("/r/r1").hasAnyAuthority("p1")//所有访问r/r1的请求,必须有p1权限
                 .antMatchers("/r/r2").hasAnyAuthority("p2")//所有访问r/r2的请求,必须有p2权限
                 .antMatchers("/r/**").authenticated()// 所有antMatchers表明的请求必须认证
                 .anyRequest().permitAll()// 除了antMatchers,其他请求可以访问
                 .and()
                 .formLogin()// 允许表单登录
-                .successForwardUrl("/login-success");// 自定义登录成功的页面地址
+                .loginPage("/login-view")// 指定我们自己的登录页面,spring security以重定向方式跳转到/login
+                .loginProcessingUrl("/login")// 指定登录处理的url,与login.jsp的action要对上,，也就是用户名、密码表单提交的目的路径
+                .successForwardUrl("/login-success")// 自定义登录成功的页面地址,在Controller里有对应的路径请求
+                 .permitAll();
     }
 }
