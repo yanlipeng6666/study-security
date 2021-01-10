3e349f9976053f0f1ebb3e0bdbaee60b985e34dd
yanlp <85827444@qq.com>
2021-01-10 23:53:24 +0800

    SpringCLoud Security 授权

---
+++ distributed‐security/distributed‐security‐order/src/main/java/com/itheima/security/distributed/order/config/TokenConfig.java
@@ -0,0 +1,21 @@
+package com.itheima.security.distributed.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+/**
+ * Program Name: distributed‐security
+ * Created by yanlp on 2021-01-10
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+@Configuration
+public class TokenConfig {
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+}
