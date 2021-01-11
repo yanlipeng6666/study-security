dfd85702dd845f8dc90048491db9b09aa6acea6e
yanlp <85827444@qq.com>
2021-01-11 11:48:12 +0800

    校验JWT令牌

--- distributed‐security/distributed‐security‐order/src/main/java/com/itheima/security/distributed/order/config/ResourceServerConfig.java
+++ distributed‐security/distributed‐security‐order/src/main/java/com/itheima/security/distributed/order/config/ResourceServerConfig.java
@@ -1,14 +1,13 @@
 package com.itheima.security.distributed.order.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
  * Program Name: distributed‐security
@@ -23,11 +22,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     public static final String RESOURCE_ID = "res1";
 
+    @Autowired
+    private TokenStore tokenStore;
+
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources
                 .resourceId(RESOURCE_ID)
-                .tokenServices(tokenService())
+                .tokenStore(tokenStore)
+                // .tokenServices(tokenService())
                 .stateless(true);
     }
 
@@ -42,13 +46,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     }
 
     //资源服务令牌解析服务
-    @Bean
-    public ResourceServerTokenServices tokenService() {
-        //使用远程服务请求授权服务器校验token,必须指定校验token 的url、client_id，client_secret
-        RemoteTokenServices service = new RemoteTokenServices();
-        service.setCheckTokenEndpointUrl("http://localhost:53020/uaa/oauth/check_token");
-        service.setClientId("c1");
-        service.setClientSecret("secret");
-        return service;
-    }
+    // @Bean
+    // public ResourceServerTokenServices tokenService() {
+    //     //使用远程服务请求授权服务器校验token,必须指定校验token 的url、client_id，client_secret
+    //     RemoteTokenServices service = new RemoteTokenServices();
+    //     service.setCheckTokenEndpointUrl("http://localhost:53020/uaa/oauth/check_token");
+    //     service.setClientId("c1");
+    //     service.setClientSecret("secret");
+    //     return service;
+    // }
 }
---
+++ distributed‐security/distributed‐security‐order/src/main/java/com/itheima/security/distributed/order/config/TokenConfig.java
@@ -0,0 +1,33 @@
+package com.itheima.security.distributed.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * Program Name: distributed‐security
+ * Created by yanlp on 2021-01-10
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+@Configuration
+public class TokenConfig {
+
+    private String SIGNING_KEY= "uaa123";
+
+    @Bean
+    public TokenStore tokenStore() {
+        // return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(SIGNING_KEY);
+        return tokenConverter;
+    }
+}
