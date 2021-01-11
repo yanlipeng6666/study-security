3ec24dacafe8769c4f8741187527ab429c2c2485
yanlp <85827444@qq.com>
2021-01-11 11:11:20 +0800

    资源服务测试

--- distributed‐security/distributed‐security‐order/src/main/java/com/itheima/security/distributed/order/config/WebSecurityConfig.java
+++ distributed‐security/distributed‐security‐order/src/main/java/com/itheima/security/distributed/order/config/WebSecurityConfig.java
@@ -1,45 +1,30 @@
 package com.itheima.security.distributed.order.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * @author Administrator
+ * Program Name: distributed‐security
+ * Created by yanlp on 2021-01-11
+ *
+ * @author yanlp
  * @version 1.0
- **/
+ */
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    //认证管理器
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    //安全拦截机制（最重要）
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/r/r1").hasAnyAuthority("p1")
-                .antMatchers("/login*").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-        ;
+                // .antMatchers("/r/r1").hasAuthority("p2")
+                // .antMatchers("/r/r2").hasAuthority("p2")
+                .antMatchers("/r/**").authenticated()// 所有/r/**的请求必须认证通过
+                .anyRequest().permitAll()// 除了/r/**,其他的请求都可以访问
 
+                ;
     }
 }
+
