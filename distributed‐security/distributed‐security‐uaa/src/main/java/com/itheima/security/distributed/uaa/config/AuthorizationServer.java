e7587b6160dddb4b3f0873bdcaf0744bc1a8a500
yanlp <85827444@qq.com>
2021-01-11 14:52:39 +0800

    完善环境配置

--- distributed‐security/distributed‐security‐uaa/src/main/java/com/itheima/security/distributed/uaa/config/AuthorizationServer.java
+++ distributed‐security/distributed‐security‐uaa/src/main/java/com/itheima/security/distributed/uaa/config/AuthorizationServer.java
@@ -6,20 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 /**
@@ -54,18 +57,41 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
                 .allowFormAuthenticationForClients();
     }
 
+    /**
+     * 客户端详情配置
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ClientDetailsService clientDetailsService(DataSource dataSource) {
+        JdbcClientDetailsService clientDetailsService = new JdbcClientDetailsService(dataSource);
+        clientDetailsService.setPasswordEncoder(passwordEncoder());
+        return clientDetailsService;
+    }
+
+
+
+
+
     // 客户端详情服务
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()// 使用in-memory存储
-                .withClient("c1")// client_id
-                .secret(new BCryptPasswordEncoder().encode("secret"))//客户端密钥
-                .resourceIds("res1")//资源列表
-                .authorizedGrantTypes("authorization_code", "password", "client_credentials", "implicit", "refresh_token")// 该client允许的授权类型authorization_code,password,refresh_token,implicit,client_credentials
-                .scopes("all")// 允许的授权范围
-                .autoApprove(false)//false跳转到授权页面
-                //加上验证回调地址
-                .redirectUris("http://www.baidu.com")
+        clients.withClientDetails(clientDetailsService);
+
+
+        // clients.inMemory()// 使用in-memory存储
+        //         .withClient("c1")// client_id
+        //         .secret(new BCryptPasswordEncoder().encode("secret"))//客户端密钥
+        //         .resourceIds("res1")//资源列表
+        //         .authorizedGrantTypes("authorization_code", "password", "client_credentials", "implicit", "refresh_token")// 该client允许的授权类型authorization_code,password,refresh_token,implicit,client_credentials
+        //         .scopes("all")// 允许的授权范围
+        //         .autoApprove(false)//false跳转到授权页面
+        //         //加上验证回调地址
+        //         .redirectUris("http://www.baidu.com")
         ;
     }
 
@@ -94,9 +120,19 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
         return service;
     }
 
+    /**
+     * 设置令牌Token的访问端点
+     * @param dataSource
+     * @return
+     */
     @Bean
-    public AuthorizationCodeServices authorizationCodeServices() {
+    public AuthorizationCodeServices authorizationCodeServices(DataSource dataSource) {
+        // 设置授权码模式的授权码如何获取
+        return new JdbcAuthorizationCodeServices(dataSource);
+
         //设置授权码模式的授权码如何 存取，暂时采用内存方式
-        return new InMemoryAuthorizationCodeServices();
+        // return new InMemoryAuthorizationCodeServices();
+
+
     }
 }
